2f3dc015cad26de97333a44a819032b5957fa6ca
tharindu <37550093+tharindu1998@users.noreply.github.com>
2020-06-03 01:17:23 +0530

    Add files via upload

---
+++ blockchain.py
@@ -0,0 +1,25 @@
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transaction =[]
+
+    def new_block(self):
+        # Creates a new Block and adds it to the chain pass
+
+
+    def new_transaction(self, sender, recipient, amount):
+
+
+    @staticmethod
+    def hash(block):
+        #Hashes a block pass
+
+    @property
+    def last_block(self):
+        # returns the last block in the chain pass
+
+
+
+
+
+
